ef230231ea727a47a9fee3045520711f4d8a19c4
github167 <catcat161-github@yahoo.com.hk>
2026-09-06 04:16:38 +0000

    6

--- jsapp.js
+++ jsapp.js
@@ -1,15 +1,13 @@
-const http = require('http');
-
+const express = require('express');
+const app = express();
 const PORT = 3000;
 
-const server = http.createServer((req, res) => {
-  // Set response headers for HTML content
-  res.writeHead(200, { 'Content-Type': 'text/html' });
-  
-  // Send the Hello World markup
-  res.end('<h1>Hello World!</h1>');
+// Define a route for the root URL
+app.get('/', (req, res) => {
+  res.send('<h1>Hello World!</h1>');
 });
 
-server.listen(PORT, () => {
-  console.log(`Server is running at http://localhost:${PORT}`);
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Express server running at http://localhost:${PORT}`);
 });
